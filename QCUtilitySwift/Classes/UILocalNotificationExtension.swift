a44072e471abbff3be1e9deffed297ca5736edf1
Qi Chen <qichen0401@gmail.com>
2016-12-15 10:28:34 +0800

    commit

---
+++ QCUtilitySwift/Classes/UILocalNotificationExtension.swift
@@ -0,0 +1,28 @@
+//
+//  UILocalNotificationExtension.swift
+//  Pods
+//
+//  Created by Qi Chen on 12/15/16.
+//
+//
+
+import Foundation
+
+extension UILocalNotification {
+    public class func registerDefaultUserNotificationSettings() {
+    UIApplication.shared.registerUserNotificationSettings(UIUserNotificationSettings(types: [.badge, .sound, .alert], categories: nil))
+    }
+    public class func scheduleNotification(fireDate: Date, repeatInterval: NSCalendar.Unit, alertBody: String) {
+        let localNotification = UILocalNotification()
+        localNotification.fireDate = fireDate
+        localNotification.repeatInterval = repeatInterval
+        localNotification.alertBody = alertBody
+        localNotification.soundName = UILocalNotificationDefaultSoundName
+        
+        UIApplication.shared.scheduleLocalNotification(localNotification)
+    }
+    
+    public class func scheduleNotification(fireDate: Date, alertBody: String) {
+        UILocalNotification.scheduleNotification(fireDate: fireDate, repeatInterval: NSCalendar.Unit(rawValue: 0), alertBody: alertBody)
+    }
+}
