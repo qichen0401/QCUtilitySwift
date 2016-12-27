2b8c4b2374f07a7a56928548f39d5771ccf79589
Qi Chen <qichen0401@gmail.com>
2016-12-27 13:17:48 +0800

    commit

--- QCUtilitySwift/Classes/TableViewController/SectionIndexTableViewController.swift
+++ QCUtilitySwift/Classes/TableViewController/SectionIndexTableViewController.swift
@@ -12,7 +12,7 @@ open class SectionIndexTableViewController: BaseTableViewController {
 
     open var rawData: [Any]! {
         didSet {
-            
+            configureDataSource()
         }
     }
     
@@ -26,14 +26,27 @@ open class SectionIndexTableViewController: BaseTableViewController {
     open func configureDataSource() {
         var dictionary: [String : [Any]] = [:]
         for object in rawData {
-            let key = name(object)
+            let key = name(object).sectionTitle()
+            if dictionary[key] == nil {
+                dictionary[key] = [object]
+            } else {
+                dictionary[key]?.append(object)
+            }
         }
-        
-        
-        
-        
-        
-        
+        var ds: [[Any]] = []
+        let sectionTitles = UILocalizedIndexedCollation.current().sectionTitles
+        for key in sectionTitles {
+            if let section = dictionary[key] {
+                ds.append(section.sorted() {
+                    if name($0).localizedStandardCompare(name($1)) == .orderedAscending {
+                        return true
+                    } else {
+                        return false
+                    }
+                })
+            }
+        }
+        dataSource = ds
     }
 
     override open func viewDidLoad() {
@@ -42,7 +55,21 @@ open class SectionIndexTableViewController: BaseTableViewController {
         // Do any additional setup after loading the view.
     }
 
+    open override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return name(dataSource[section].first).sectionTitle()
+    }
 
+    open override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        var sectionIndexTitles: [String] = []
+        for section in dataSource {
+            sectionIndexTitles.append(name(section.first).sectionTitle())
+        }
+        return sectionIndexTitles
+    }
+    
+    open override func tableView(_ tableView: UITableView, sectionForSectionIndexTitle title: String, at index: Int) -> Int {
+        return index
+    }
 
 }
 
