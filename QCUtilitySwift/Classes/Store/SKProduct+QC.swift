2ac423aa09af18e17a145e76867a77d24979f840
Qi Chen <qichen0401@gmail.com>
2017-01-14 16:27:14 +0800

    commit

---
+++ QCUtilitySwift/Classes/Store/SKProduct+QC.swift
@@ -0,0 +1,22 @@
+//
+//  SKProduct+QC.swift
+//  Pods
+//
+//  Created by Qi Chen on 1/14/17.
+//
+//
+
+import Foundation
+import StoreKit
+
+extension SKProduct {
+    var formattedPrice: String {
+        get {
+            let numberFormatter = NumberFormatter()
+            numberFormatter.formatterBehavior = .behavior10_4
+            numberFormatter.numberStyle = .currency
+            numberFormatter.locale = self.priceLocale
+            return numberFormatter.string(from: self.price) ?? ""
+        }
+    }
+}
