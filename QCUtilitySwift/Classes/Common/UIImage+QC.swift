ff2d675fcc4888b213d5361834710d5c451cf7d1
Qi Chen <qichen0401@gmail.com>
2017-02-28 16:06:25 +0800

    commit

--- QCUtilitySwift/Classes/Common/UIImage+QC.swift
+++ QCUtilitySwift/Classes/Common/UIImage+QC.swift
@@ -16,4 +16,28 @@ extension UIImage {
         let cgImage = context.createCGImage(CIImage(cvPixelBuffer: imageBuffer), from: CGRect(x: 0, y: 0, width: CGFloat(CVPixelBufferGetWidth(imageBuffer)), height: CGFloat(CVPixelBufferGetHeight(imageBuffer))))
         self.init(cgImage: cgImage!)
     }
+    
+    public func write(to url: URL) {
+        do {
+            try UIImagePNGRepresentation(self)?.write(to: url)
+            let userDefaults = UserDefaults.standard
+            userDefaults.set(self.imageOrientation.rawValue, forKey: "UIImage+QC_imageOrientation_\(url.absoluteString)")
+            userDefaults.synchronize()
+        } catch {
+            print(error)
+        }
+        
+    }
+    
+    public class func load(from url: URL) -> UIImage? {
+        do {
+            let data = try Data(contentsOf: url)
+            let userDefaults = UserDefaults.standard
+            return UIImage(cgImage: UIImage(data: data)!.cgImage!, scale: 1.0, orientation: UIImageOrientation(rawValue: userDefaults.integer(forKey: "UIImage+QC_imageOrientation_\(url.absoluteString)"))!)
+        } catch {
+            print(error)
+        }
+        return nil
+    }
+    
 }
