9977a838474a5d85cf4173534a68794ffe9eb891
Qi Chen <qichen0401@gmail.com>
2017-06-14 14:01:16 +0800

    commit

--- QCUtilitySwift/Classes/TableViewController/BaseTableViewController.swift
+++ QCUtilitySwift/Classes/TableViewController/BaseTableViewController.swift
@@ -14,32 +14,16 @@ open class BaseTableViewController: UITableViewController {
         public static let reuseIdentifier = "reuseIdentifier"
     }
     
-    open var emptyTableFooterView: UIView? {
-        didSet {
-            emptyTableFooterView?.frame = self.view.bounds
-            
-            configureTableFooterView()
-        }
-    }
-    
     override open func viewDidLoad() {
         super.viewDidLoad()
         
         registerForCellReuseIdentifier()
         
-        configureTableFooterView()
+        tableView.tableFooterView = UIView()
     }
     
     open func registerForCellReuseIdentifier() {
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: Constants.reuseIdentifier)
-    }
-    
-    func configureTableFooterView() {
-        if tableView.numberOfSections == 0 {
-            tableView.tableFooterView = emptyTableFooterView ?? UIView()
-        } else {
-            tableView.tableFooterView = UIView()
-        }
     }
     
     override open func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -58,60 +42,5 @@ open class BaseTableViewController: UITableViewController {
     override open func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
