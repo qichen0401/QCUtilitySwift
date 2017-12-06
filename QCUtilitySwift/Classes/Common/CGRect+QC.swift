f3ae67b6638427b4a49ce2ad627e0f01e7ae193f
Qi Chen <qichen0401@gmail.com>
2017-12-06 17:24:50 +0800

    c

--- QCUtilitySwift/Classes/Common/CGRect+QC.swift
+++ QCUtilitySwift/Classes/Common/CGRect+QC.swift
@@ -9,6 +9,14 @@
 import Foundation
 
 extension CGRect {
+    public var center: CGPoint {
+        set {
+            self.origin = CGPoint(x: center.x - self.size.width/2, y: center.y - self.size.height/2)
+        }
+        get {
+            return CGPoint(x: self.origin.x + self.size.width/2, y: self.origin.y + self.size.height/2)
+        }
+    }
     public init(center: CGPoint, size: CGSize) {
         let origin = CGPoint(x: center.x - size.width / 2, y: center.y - size.height / 2)
         self.init(origin: origin, size: size)
