98c53da30b9545f3cdb19a9db6e430165aa30331
Qi Chen <qichen0401@gmail.com>
2017-12-12 14:34:50 +0800

    c

--- QCUtilitySwift/Classes/Common/CGRect+QC.swift
+++ QCUtilitySwift/Classes/Common/CGRect+QC.swift
@@ -17,9 +17,21 @@ extension CGRect {
             return CGPoint(x: self.origin.x + self.size.width/2, y: self.origin.y + self.size.height/2)
         }
     }
+    
     public init(center: CGPoint, size: CGSize) {
         let origin = CGPoint(x: center.x - size.width / 2, y: center.y - size.height / 2)
         self.init(origin: origin, size: size)
+    }
+    
+    public func ellipseContains(point: CGPoint) -> Bool {
+        let x = point.x
+        let y = point.y
+        let h = self.center.x
+        let k = self.center.y
+        let a = self.width/2
+        let b = self.height/2
+        let result = (x - h)*(x - h)/(a*a) + (y-k)*(y-k)/(b*b)
+        return result <= 1
     }
     
 }
