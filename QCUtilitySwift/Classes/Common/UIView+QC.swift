4fe3f842cbfbdfcdbe921b68e3c1b8019d55422d
Qi Chen <qichen0401@gmail.com>
2017-12-15 15:11:40 +0800

    c

--- QCUtilitySwift/Classes/Common/UIView+QC.swift
+++ QCUtilitySwift/Classes/Common/UIView+QC.swift
@@ -9,6 +9,19 @@
 import Foundation
 
 extension UIView {
+    
+    @available(iOS 11.0, *)
+    public func fill(in view: UIView) {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.removeConstraints(self.constraints)
+        NSLayoutConstraint.activate([
+            view.leftAnchor.constraint(equalTo: self.leftAnchor),
+            view.rightAnchor.constraint(equalTo: self.rightAnchor),
+            view.topAnchor.constraint(equalTo: self.topAnchor),
+            view.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            ])
+    }
+    
     public func configureBorder(borderWidth: CGFloat, borderColor: CGColor, cornerRadius: CGFloat) {
         self.layer.borderWidth = borderWidth
         self.layer.borderColor = borderColor
